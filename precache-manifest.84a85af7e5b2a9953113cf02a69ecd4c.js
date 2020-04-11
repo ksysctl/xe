5efd7511428bc633620efca810b9b3907420761e
Moises Brenes <moises.brenes@gmail.com>
2020-04-11 15:24:27 -0600

    Updates

---
+++ precache-manifest.84a85af7e5b2a9953113cf02a69ecd4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc675145aa326034cdd5eb2672bfd94",
+    "url": "/xe/index.html"
+  },
+  {
+    "revision": "ca53e623a77c2e982042",
+    "url": "/xe/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "b5a952ff87d447c2b7d4",
+    "url": "/xe/static/css/main.fff9e578.chunk.css"
+  },
+  {
+    "revision": "ca53e623a77c2e982042",
+    "url": "/xe/static/js/2.5ab1ebb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xe/static/js/2.5ab1ebb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a952ff87d447c2b7d4",
+    "url": "/xe/static/js/main.f46b4b09.chunk.js"
+  },
+  {
+    "revision": "370dad855efa64b5776a",
+    "url": "/xe/static/js/runtime-main.15f6ce0c.js"
+  },
+  {
+    "revision": "c8582efac825fc1a962618fbfb8a034f",
+    "url": "/xe/static/media/logo.c8582efa.svg"
+  }
+]);
